c1d55cb42a8a0fbf57052716642e938e5fc5e128
Matt Katz <mykatz01@gmail.com>
2020-02-07 16:41:36 -0800

    change

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
-import AdminLayout from "layouts/Admin.jsx";
-import AuthLayout from "layouts/Auth.jsx";
+import AdminLayout from "./layouts/Admin.jsx";
+import AuthLayout from "./layouts/Auth.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
