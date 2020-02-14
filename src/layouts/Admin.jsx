04d4b05655a6a9e98b5d25cf4ca81133dd6ca8cc
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-13 22:24:44 -0800

    fix location of components

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -3,9 +3,9 @@ import { Route, Switch } from "react-router-dom";
 // reactstrap components
 import { Container } from "reactstrap";
 // core components
-import AdminNavbar from "components/Navbars/AdminNavbar.jsx";
-import AdminFooter from "components/Footers/AdminFooter.jsx";
-import Sidebar from "components/Sidebar/Sidebar.jsx";
+import AdminNavbar from "../components/Navbars/AdminNavbar.jsx";
+import AdminFooter from "../components/Footers/AdminFooter.jsx";
+import Sidebar from "../components/Sidebar/Sidebar.jsx";
 
 import routes from "../routes";
 
